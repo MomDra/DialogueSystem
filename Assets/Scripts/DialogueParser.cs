9888ada07224d5774a205639c46cc54a934b9047
MomDra <yem9087@koreatech.ac.kr>
2022-12-05 15:16:31 +0900

    Init dialogue system project

---
+++ Assets/Scripts/DialogueParser.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueParser : MonoBehaviour
+{
+    public Dialogue[] Parse(string _CSVFileName)
+    {
+        List<Dialogue> dialogueList = new List<Dialogue>();
+    }
+}
